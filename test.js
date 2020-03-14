21a3d7d3bf8d76c71824fdfb7a92a7ce0ddb77e9
Dylan Libby <libbydylan10@gmail.com>
2020-03-14 11:02:23 -0400

    testing commit

---
+++ test.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+import "Test.css"
+
+export default class Testing extends Component{
+  render(){
+    return(
+      <h1 className="Header">Testing</h1>
+    )
+  }
+}
